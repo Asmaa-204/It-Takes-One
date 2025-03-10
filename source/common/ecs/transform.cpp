a99554dde86c29915c0a0812d59feec5f78b0ac7
akramhany <akramhany65@gmail.com>
2025-03-10 03:29:14 +0200

    Feat: Implement transformation (not working though haha)

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -1,23 +1,35 @@
-#include "entity.hpp"
 #include "../deserialize-utils.hpp"
+#include "entity.hpp"
 
 #include <glm/gtx/euler_angles.hpp>
 
 namespace our {
 
-    // This function computes and returns a matrix that represents this transform
-    // Remember that the order of transformations is: Scaling, Rotation then Translation
-    // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
-    glm::mat4 Transform::toMat4() const {
-        //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
-    }
+// This function computes and returns a matrix that represents this transform
+// Remember that the order of transformations is: Scaling, Rotation then
+// Translation HINT: to convert euler angles to a rotation matrix, you can use
+// glm::yawPitchRoll
+glm::mat4 Transform::toMat4() const {
+  // TODO: (Req 3) Write this function
 
-     // Deserializes the entity data and components from a json object
-    void Transform::deserialize(const nlohmann::json& data){
-        position = data.value("position", position);
-        rotation = glm::radians(data.value("rotation", glm::degrees(rotation)));
-        scale    = data.value("scale", scale);
-    }
+  // Create scaling matrix
+  glm::mat4 scaleMat = glm::scale(glm::mat4(1.0f), scale);
 
+  // Create rotation matrix from Euler angles (yaw, pitch, roll)
+  glm::mat4 rotationMat = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+
+  // Create translation matrix
+  glm::mat4 translationMat = glm::translate(glm::mat4(1.0f), position);
+
+  // Combine transformations: Scaling → Rotation → Translation
+  return translationMat * rotationMat * scaleMat;
 }
+
+// Deserializes the entity data and components from a json object
+void Transform::deserialize(const nlohmann::json &data) {
+  position = data.value("position", position);
+  rotation = glm::radians(data.value("rotation", glm::degrees(rotation)));
+  scale = data.value("scale", scale);
+}
+
+} // namespace our
