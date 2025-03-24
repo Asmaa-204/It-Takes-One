9efeb26bbb677dc415cef69b0747679d5458a12c
asmaa-204 <asmaa.abozaid204@gmail.com>
2025-03-25 00:40:32 +0200

    feat: finished EntityComponentSystem

--- source/common/ecs/entity.cpp
+++ source/common/ecs/entity.cpp
@@ -12,7 +12,15 @@ namespace our {
     // its parent's parent's matrix and so on till you reach the root.
     glm::mat4 Entity::getLocalToWorldMatrix() const {
         //TODO: (Req 8) Write this function
-        return glm::mat4(1.0f);
+       glm::mat4 matrix = localTransform.toMat4();
+        
+        Entity* current = parent;
+        while(current) {
+            matrix = current->localTransform.toMat4() * matrix;
+            current = current->parent;
+        }
+        
+        return matrix;
     }
 
     // Deserializes the entity data and components from a json object
